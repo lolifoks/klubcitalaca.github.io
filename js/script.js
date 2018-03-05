2f5c40b3f07c652f4602f52602b9b09c8c329d03
Marija Lekic <marija.lekic91@gmail.com>
2018-03-05 22:44:59 +0100

    quotecommit

--- js/script.js
+++ js/script.js
@@ -331,52 +331,42 @@ greskeDiv.innerHTML=greskeLista;
   $(document).ready(function() {
     colorNum = 1; 
     var quotes = [{
-      author: "Dr. Seuss",
-      quote: "Don't cry because it's over, smile because it happened."
+      author: "Stendal",
+      quote: "Dobra knjiga je u mom životu događaj."
     }, {
-      author: "Oscar Wilde", 
-      quote: "Be yourself; everyone else is already taken."
+      author: "Margaret Atvud", 
+      quote: "Reč, pa reč, pa reč - to je moć. "
     }, {
-      author: "Friedrich Nietzche", 
-      quote: "He who has a why to live can bear any how." 
+      author: "Ursula K. Le Guin", 
+      quote: "Čitamo da bi smo saznali ko smo. Šta  drugi ljudi, izmišljeni ili stvarni, rade, razmišljaju i osećaju - to je suštinski pokazatelj ko smo mi sami i šta možemo postati. " 
     }, {
-      author: "André Gide", 
-      quote: "It is better to be hated for what you are than to be loved for what you are not."
+      author: "J.K. Rouling", 
+      quote: "Ako ne voliš da čitaš samo nisi pronašao pravu knjigu. "
     }, { 
-      author: "Albert Einstein", 
-      quote:"There are only two ways to live your life. One is as though nothing is a miracle. The other is as though everything is a miracle."
+      author: "Vilijam Nikolson", 
+      quote:"Čitamo jer tako znamo da nismo sami."
     }, {
-      author: "Dr. Seuss", 
-      quote: "I like nonsense, it wakes up the brain cells. Fantasy is a necessary ingredient in living."
+      author: " Stiven King", 
+      quote: "Knjige su jedinstvena, prenosiva magija."
     }, {
-      author: "Mark Twain", 
-      quote: "Never put off till tomorrow what may be done day after tomorrow just as well."
+      author: "Čarls Vilijam Eliot", 
+      quote: "Knjige su najtiši i najodaniji prijatelji, one su najdostupniji i najmudriniji savetnici i najstrpljiviji učitelji. "
     }, {
-      author: "Douglas Adams", 
-      quote: "I love deadlines. I love the whooshing noise they make as they go by."
-    }, {
-      author: "Theodore Roosevelt", 
-      quote: "Do what you can, with what you have, where you are."
-    }, {
-      author: "Rumi", 
-      quote: "Let the beauty of what you love be what you do."
-    }, {
-      author: "Khalil Gibran", 
-      quote: "Your pain is the breaking of the shell that encloses your understanding."
-    }, {
-      author: "Søren Kierkegaard", 
-      quote: "Life is not a problem to be solved, but a reality to be experienced."
-    }, {
-      author: "Immanuel Kant", 
-      quote: "Immaturity is the incapacity to use one's intelligence without the guidance of another."
-    }];
+      author: "Marsel Prust", 
+      quote: "Čitanje je čudo komunikacije čak i u samom središtu samoće."
+    },
+	{
+      author: "Italo Kalvino", 
+      quote: "Ono što je zajedničko vođenju ljubavi i čitanju jeste stvaranje vremena i prostora, drugačijih, nemerljivih vremena i prostora."
+    }
+	];
     function getRandomInt(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min)) + min; 
 }
     function changeColor(){
-        var colors = ['#16a085', '#27ae60', '#2c3e50', '#f39c12', '#e74c3c', '#9b59b6', '#FB6964', '#342224', "#472E32", "#BDBB99", "#77B1A9", "#73A857"];
+        var colors = ['#16a085', '#27ae60', '#f39c12', '#e74c3c', '#9b59b6', '#FB6964', "#BDBB99", "#77B1A9", "#73A857"];
         if(colorNum == colors.length)
           colorNum = 0; 
       $(".text").css("transition","color 3s ease" );
